3bc60d68a41da544cc5d6683d69e41d9800586e5
tommylohz <tommylohz@gmail.com>
2022-06-11 18:22:37 +0800

    feat: add arrays solutions

---
+++ Javascript Objects/Check if property exists in object.js
@@ -0,0 +1,7 @@
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
+
+function myFunction(a, b) {
+  return b in a
+}
---
+++ Javascript Objects/Creating Javascript objects one.js
@@ -0,0 +1,7 @@
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+
+function myFunction(a) {
+  return { key: a }
+}
